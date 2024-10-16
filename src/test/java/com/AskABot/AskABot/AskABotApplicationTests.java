2dcafd5c4584003743b62ada5aaddae4eb5d6c18
dennis <isacsson_89@hotmail.com>
2024-10-16 09:01:36 +0200

    CleanSpring

---
+++ src/test/java/com/AskABot/AskABot/AskABotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AskABot.AskABot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskABotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
